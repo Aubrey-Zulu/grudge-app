a4842b681d9a8862f0766f695fcbbe783ee2dde2
Aubrey Zulu <aubreyzulu@Aubreys-MBP.local.tld>
2020-12-06 12:17:33 +0200

    implemented custom reducer

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import id from 'uuid/v4';
 
@@ -7,23 +7,52 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
+const markForgiven = (state, payload) => {
+  return state.map((grudge) => {
+    if (grudge.id === payload.id) {
+      return { ...grudge, forgiven: !grudge.forgiven };
+    }
+    return grudge;
+  });
+};
+
+//  Grudge Actions
+const GRUDGE_ADD = 'GRUDGE_ADD';
+const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
+
+// Grudge  Reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case GRUDGE_ADD:
+      return [...state, action.payload];
+
+    case GRUDGE_FORGIVE:
+      return markForgiven(state, action.payload);
+
+    default:
+      return state;
+  }
+};
+
 const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+  const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = grudge => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
-  };
+  const addGrudge = useCallback(
+    ({ person, reason }) => {
+      dispatch({
+        type: GRUDGE_ADD,
+        payload: { person, reason, forgiven: false, id: id() }
+      });
+    },
+    [dispatch]
+  );
 
-  const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
-  };
+  const toggleForgiveness = useCallback(
+    (id) => {
+      dispatch({ type: GRUDGE_FORGIVE, payload: { id } });
+    },
+    [dispatch]
+  );
 
   return (
     <div className="Application">
